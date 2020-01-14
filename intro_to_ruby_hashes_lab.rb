4abcb1dec8b623ee75981e093941bd690157929e
Paul Kim <pkim1295@gmail.com>
2020-01-14 21:56:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{railroads: {}}
+	{:railroads => {}}
 end
 
 def monopoly_with_second_tier
-  {railroads:
+  {:railroads =>
     {
-      pieces: 4
+      :pieces => 4
     }
     
   }
